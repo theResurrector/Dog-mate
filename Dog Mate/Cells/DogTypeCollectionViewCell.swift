74c05fe9c88bea6ddbde34e4f1d1f959e1800174
Anson <chang@sephora.sg>
2022-09-18 15:24:38 +0800

    chore: added logged in view controller and related cell

---
+++ Dog Mate/Cells/DogTypeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DogTypeCollectionViewCell.swift
+//  Dog Mate
+//
+//  Created by NG Anson on 18/9/22.
+//
+
+import UIKit
+
+class DogTypeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
