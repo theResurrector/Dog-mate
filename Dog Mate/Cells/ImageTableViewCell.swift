4f9f6c585418e61d8ec5501e948bb282310181e5
Anson <chang@sephora.sg>
2022-09-19 00:00:53 +0800

    chore: added new files

---
+++ Dog Mate/Cells/ImageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ImageTableViewCell.swift
+//  Dog Mate
+//
+//  Created by NG Anson on 18/9/22.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Dog Mate/Cells/TitleSubtitleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TitleSubtitleTableViewCell.swift
+//  Dog Mate
+//
+//  Created by NG Anson on 18/9/22.
+//
+
+import UIKit
+
+class TitleSubtitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
