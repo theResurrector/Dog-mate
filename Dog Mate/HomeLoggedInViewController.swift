3fca109c29a9974ad300d4c38518664d925e469b
Anson <chang@sephora.sg>
2022-09-18 18:07:37 +0800

    feat: display number of pets from database

--- Dog Mate/HomeLoggedInViewController.swift
+++ Dog Mate/HomeLoggedInViewController.swift
@@ -7,9 +7,12 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 
 class HomeLoggedInViewController: UIViewController {
-    var data: [String] = []
+    var petData: [Pet] = []
+    let reference = Database.database(url: "https://dog-mate-e7f92-default-rtdb.asia-southeast1.firebasedatabase.app").reference()
+    
     @IBOutlet weak var collectionView: UICollectionView! {
         didSet {
             self.collectionView.register(UINib(nibName: "DogTypeCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "DogTypeCollectionViewCell")
@@ -17,6 +20,7 @@ class HomeLoggedInViewController: UIViewController {
             self.collectionView.dataSource = self
         }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,6 +28,8 @@ class HomeLoggedInViewController: UIViewController {
         navigationItem.setLeftBarButton(logoutButton, animated: true)
         let addPetButton = UIBarButtonItem(title: "Add pet", style: .plain, target: self, action: #selector(addPet))
         navigationItem.setRightBarButton(addPetButton, animated: true)
+        
+        fetchData()
     }
     
     @objc private func logout() {
@@ -44,12 +50,53 @@ class HomeLoggedInViewController: UIViewController {
     }
 
     func fetchData() {
+        let petRef = self.reference.child("/pets")
+        petRef.observe(.value) {[weak self] snapshot in
+            guard let weakself = self else { return }
+            if let value = snapshot.value as? [String: Any] {
+                weakself.petData.append(contentsOf: weakself.convertToData(data: value))
+                weakself.collectionView.reloadData()
+            }
+        }
+    }
+    
+    func convertToData(data: [String: Any]) -> [Pet] {
+        var pets: [Pet] = []
+        for pet in data {
+            if let petData = pet.value as? [String: Any] {
+                var petObject = Pet()
+                if let age = petData["age"] as? Int {
+                    petObject.age = age
+                }
+                if let breed = petData["breed"] as? String {
+                    petObject.breed = breed
+                }
+                if let desc = petData["desc"] as? String {
+                    petObject.desc = desc
+                }
+                if let image = petData["image"] as? String {
+                    petObject.image = image
+                }
+                if let dateCreated = petData["dateCreated"] as? String {
+                    petObject.date_created = dateCreated
+                }
+                if let vaccination = petData["vaccination"] as? NSNumber {
+                    petObject.vaccination = vaccination.boolValue
+                }
+                if let owner = petData["owner"] as? String {
+                    petObject.owner = owner
+                }
+                pets.append(petObject)
+            }
+        }
+        pets = pets.filter { $0.owner != Auth.auth().currentUser?.uid}
+        return pets
     }
 }
 
 extension HomeLoggedInViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return petData.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
---
+++ Dog Mate/Pet.swift
@@ -0,0 +1,18 @@
+//
+//  Pet.swift
+//  Dog Mate
+//
+//  Created by NG Anson on 18/9/22.
+//
+
+import Foundation
+
+struct Pet {
+    var age: Int?
+    var breed: String?
+    var desc: String?
+    var image: String?
+    var date_created: String?
+    var owner: String?
+    var vaccination: Bool = false
+}
